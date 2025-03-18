3c0690c1af8b47e43442574f0cbb0338d9d36dda
Ivan Revko <revko7692@gmail.com>
2025-03-19 00:02:34 +0200

    Finally changes for Buddy

--- db/models.py
+++ db/models.py
@@ -73,7 +73,7 @@ class Order(models.Model):
         ordering = ["-created_at"]
 
     def __str__(self) -> str:
-        return f"{str(self.created_at)}"
+        return str(self.created_at)
 
 
 class Ticket(models.Model):
